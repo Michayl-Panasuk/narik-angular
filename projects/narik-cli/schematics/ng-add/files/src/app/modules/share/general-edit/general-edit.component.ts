5d7b3e10d7e7f4160ccbffeec7e470034b807f9a
ali <abadakhshan@gmail.com>
2020-12-02 07:39:37 +0330

    rename DynamicForm to DynamicComponent

--- projects/narik-cli/schematics/ng-add/files/src/app/modules/share/general-edit/general-edit.component.ts
+++ projects/narik-cli/schematics/ng-add/files/src/app/modules/share/general-edit/general-edit.component.ts
@@ -1,9 +1,9 @@
 import { Component, Injector, forwardRef } from '@angular/core';
 import { NarikUiEditForm } from '@narik/ui-lib';
-import { DynamicForm } from '@narik/core';
+import { DynamicComponent } from '@narik/core';
 import { HOST_TOKEN } from '@narik/infrastructure';
 
-@DynamicForm('GeneralEditComponent')
+@DynamicComponent('GeneralEditComponent')
 @Component({
   templateUrl: 'general-edit.component.html',
   providers: [
--- projects/narik-cli/schematics/ng-add/files/src/app/modules/share/general-list/@devextreme@general-list.component.ts
+++ projects/narik-cli/schematics/ng-add/files/src/app/modules/share/general-list/@devextreme@general-list.component.ts
@@ -1,8 +1,8 @@
 import { NarikUiListForm } from '@narik/ui-lib';
 import { Component, forwardRef, Injector } from '@angular/core';
-import { DynamicForm } from '@narik/core';
+import { DynamicComponent } from '@narik/core';
 import { HOST_TOKEN } from '@narik/infrastructure';
-@DynamicForm('GeneralListComponent')
+@DynamicComponent('GeneralListComponent')
 @Component({
   templateUrl: 'general-list.component.html',
   providers: [
